e8df8c33998eba2e98ca46629be5378f154fbe0d
elmtang <hao.tang@gmail.com>
2025-02-07 10:41:54 -0500

    create a new branch part3-challenge

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -3,6 +3,8 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Git and VS code Demo");
 
-Console.WriteLine("plan to add a branch for challenge project - part 3");
+Console.WriteLine("plan to add a branch for challenge project - part 2");
 
 Console.WriteLine("plan to add a branch for guided project - part 3");
+
+Console.WriteLine("plan to add a branch for challenge project - part 3");
