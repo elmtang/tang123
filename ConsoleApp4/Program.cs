bcd8dad47e1f4f56b8c6327b521bde62c962caf4
elmtang <hao.tang@gmail.com>
2025-02-07 10:39:24 -0500

    branch part3-guided

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,3 +4,5 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Git and VS code Demo");
 
 Console.WriteLine("plan to add a branch for challenge project - part 3");
+
+Console.WriteLine("plan to add a branch for guided project - part 3");
